8aa626d3f2d6946d808bc0cb441ca6f5d53150cc
Nikolas Santis <nikosantis@gmail.com>
2019-11-16 18:57:31 -0300

    useLocalStorage and useNearScreen customs hook

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,56 +1,16 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 
 import { Article, ImgWrapper, Img, Button } from './styles'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { useNearScreen } from '../../hooks/useNearScreen'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
-function useLocalStorage (key, initialValue) {
-  const [storedValue, setValue] = useState(() => {
-    try {
-      const item = window.localStorage.getItem(key)
-      return item !== null ? JSON.parse(item) : initialValue
-    } catch (e) {
-      return initialValue
-    }
-  })
-
-  const setLocalStorage = value => {
-    try {
-      window.localStorage.setItem(key, JSON.stringify(value))
-      setValue(value)
-    } catch (e) {
-      console.log(e)
-    }
-  }
-
-  return [storedValue, setLocalStorage]
-}
-
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const element = useRef(null)
-  const [show, setShow] = useState(false)
+  const [show, element] = useNearScreen()
   const key = `like-${id}`
   const [liked, setLiked] = useLocalStorage(key, false)
-
-  useEffect(function () {
-    Promise.resolve(
-      typeof window.IntersectionObserver !==
-    'undefined'
-        ? window.IntersectionObserver
-        : import('intersection-observer')
-    ).then(() => {
-      const observer = new
-      window.IntersectionObserver(function (entries) {
-        const { isIntersecting } = entries[0]
-        if (isIntersecting) {
-          setShow(true)
-          observer.disconnect()
-        }
-      })
-      observer.observe(element.current)
-    })
-  }, [element])
 
   const Icon = liked ? MdFavorite : MdFavoriteBorder
 
