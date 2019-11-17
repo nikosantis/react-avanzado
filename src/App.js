a62fcfed85bb772e78e5c200d3e09628749cfdc3
Nikolas Santis <nikosantis@gmail.com>
2019-11-16 23:37:14 -0300

    Protected Routes

--- src/App.js
+++ src/App.js
@@ -6,8 +6,15 @@ import { NavBar } from './components/NavBar'
 
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 
 import { Router } from '@reach/router'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 export const App = () => {
   return (
@@ -19,6 +26,24 @@ export const App = () => {
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? (
+                <Router>
+                  <Favs path='/favs' />
+                  <User path='/user' />
+                </Router>
+              )
+              : (
+                <Router>
+                  <NotRegisteredUser path='/favs' />
+                  <NotRegisteredUser path='/user' />
+                </Router>
+              )
+        }
+      </UserLogged>
       <NavBar />
     </div>
   )
