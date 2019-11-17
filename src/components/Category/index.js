91f2e227ac25cfed61a8bd3912ab8a302c6d6619
Nikolas Santis <nikosantis@gmail.com>
2019-11-16 22:25:43 -0300

    Using Link

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { Anchor, Image, SquareLoading, SquareInside } from './style'
+import { Link, Image, SquareLoading, SquareInside } from './style'
 
 const DEFAULT_IMAGE = 'https://i.imgur.com/dJa0Hpl.jpg'
 
-export const Category = ({ cover = DEFAULT_IMAGE, path, emoji = '?' }) => (
-  <Anchor href={path}>
+export const Category = ({ cover = DEFAULT_IMAGE, path = '#', emoji = '?' }) => (
+  <Link to={path}>
     <Image src={cover} />
     {emoji}
-  </Anchor>
+  </Link>
 )
 
 export const Loading = () => {
