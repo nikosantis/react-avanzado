82b15166afc735bdc6ba95e8c683f8c66d303c66
Nikolas Santis <nikosantis@gmail.com>
2019-11-18 17:42:35 -0300

    Using PropTypes

--- src/components/FavButton/index.js
+++ src/components/FavButton/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button } from './styles'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import PropTypes from 'prop-types'
 
 export const FavButton = ({ liked, likes, onClick }) => {
   const Icon = liked ? MdFavorite : MdFavoriteBorder
@@ -10,4 +11,10 @@ export const FavButton = ({ liked, likes, onClick }) => {
       <Icon size='32px' /> {likes} likes!
     </Button>
   )
+}
+
+FavButton.propTypes = {
+  liked: PropTypes.bool.isRequired,
+  likes: PropTypes.number,
+  onClick: PropTypes.func
 }
--- src/components/ListOfFavs/index.js
+++ src/components/ListOfFavs/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Grid, Image, Link } from './styles'
+import PropTypes from 'prop-types'
 
 export const ListOfFavs = ({ favs = [] }) => {
   return (
@@ -12,5 +13,14 @@ export const ListOfFavs = ({ favs = [] }) => {
         )
       }
     </Grid>
+  )
+}
+
+ListOfFavs.propTypes = {
+  favs: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      src: PropTypes.string.isRequired
+    })
   )
 }
