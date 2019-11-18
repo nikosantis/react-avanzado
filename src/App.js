2bd87a57e9d4b3e5f73b22b974b64b8324e80b53
Nikolas Santis <nikosantis@gmail.com>
2019-11-18 13:57:43 -0300

    Finish Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
@@ -9,38 +9,30 @@ import { Detail } from './pages/Detail'
 import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
+import { NotFound } from './pages/NotFound'
 
-import { Router } from '@reach/router'
-import Context from './Context'
+import { Router, Redirect } from '@reach/router'
+import { Context } from './Context'
 
 export const App = () => {
+  const { isAuth } = useContext(Context)
   return (
     <div>
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path='/' />
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
+        {!isAuth && <NotRegisteredUser path='/login' />}
+        {!isAuth && <Redirect from='/favs' to='/login' />}
+        {!isAuth && <Redirect from='/user' to='/login' />}
+        {isAuth && <Redirect from='/login' to='/' />}
+
+        <Favs path='/favs' />
+        <User path='/user' />
       </Router>
-      <Context.Consumer>
-        {
-          ({ isAuth }) =>
-            isAuth
-              ? (
-                <Router>
-                  <Favs path='/favs' />
-                  <User path='/user' />
-                </Router>
-              )
-              : (
-                <Router>
-                  <NotRegisteredUser path='/favs' />
-                  <NotRegisteredUser path='/user' />
-                </Router>
-              )
-        }
-      </Context.Consumer>
       <NavBar />
     </div>
   )
---
+++ src/pages/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const NotFound = () => (
+  <h1>Esta página no existe :(</h1>
+)
