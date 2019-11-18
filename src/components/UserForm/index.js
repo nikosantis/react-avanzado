5771f69cbfbaed8265e006bbd9bf44f6c20f4c11
Nikolas Santis <nikosantis@gmail.com>
2019-11-17 22:01:26 -0300

    Form style

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { useInputValue } from '../../hooks/useInputvalue'
+import { Form, Input, Button, Title } from './styles'
 
-export const UserForm = ({ onSubmit }) => {
+export const UserForm = ({ onSubmit, title }) => {
   const email = useInputValue('')
   const password = useInputValue('')
 
   return (
-    <form onSubmit={onSubmit}>
-      <input placeholder='Email' {...email} />
-      <input placeholder='Password' type='password' {...password} />
-      <button>Iniciar Sesión</button>
-    </form>
+    <>
+      <Title>{title}</Title>
+      <Form onSubmit={onSubmit}>
+        <Input placeholder='Email' {...email} />
+        <Input placeholder='Password' type='password' {...password} />
+        <Button>{title}</Button>
+      </Form>
+    </>
   )
 }
--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -7,7 +7,10 @@ export const NotRegisteredUser = () => (
     {
       ({ activateAuth }) => {
         return (
-          <UserForm onSubmit={activateAuth} />
+          <>
+            <UserForm title='Registrarse' onSubmit={activateAuth} />
+            <UserForm title='Iniciar Sesión' onSubmit={activateAuth} />
+          </>
         )
       }
     }
