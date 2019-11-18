0bcdc586ecff5dd14724a3d342628b82811ae312
Nikolas Santis <nikosantis@gmail.com>
2019-11-18 12:55:32 -0300

    Close Session

---
+++ src/components/SubmitButton/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Button } from './styles'
+
+export const SubmitButton = ({ children, disabled, onClick }) => {
+  return (
+    <Button disabled={disabled} onClick={onClick}>{children}</Button>
+  )
+}
